4e2edad7531de08cb02b8db059bec999bf6f6500
waltereidi <walter-eidi@hotmail.com>
2023-12-28 21:28:57 -0300

    Adicionado pacotes de extensão Axios , Vuelidator , Moment. Removido pasta node-modules. falta validar corretamente o gitignore

--- front/src/views/SegundaPagina.vue
+++ front/src/views/SegundaPagina.vue
@@ -1,17 +1,20 @@
 <script lang="ts">
 import { appStore } from '@/stores/appStore'
-export default{
-        data(){
-            return {
-                appStore : appStore() , 
-            }
+export default {
+    data() {
+        return {
+            appStore: appStore(),
         }
     }
+}
 </script>
 <template>
     <h1>Segunda Pagina</h1>
     <h3>{{ appStore.getParametroArmazenado }}</h3>
-</template>
-<style>
 
-</style>
+    <ul>
+        <li v-if="Object.keys(appStore.getDadosCep).length !== 0" v-for="item in Object.keys(appStore.getDadosCep)">{{
+            appStore.getDadosCep[item] }}</li>
+    </ul>
+</template>
+<style></style>
